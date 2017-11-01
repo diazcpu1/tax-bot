1f01c1bcb367a7edf37a75480e178463fa89d08c
Axel <diaz.cpu1@gmail.com>
2017-10-31 20:44:01 -0400

    Create TaxBot.java
    Class for the TaxBot object, which does most of the work in this app

---
+++ TaxBot.java
@@ -0,0 +1,19 @@
+package com.roboworld;
+
+public class TaxBot {
+
+	// original user price
+	private double userPrice;
+
+	// setter for userPrice
+	public void setPrice(double price) {
+		this.userPrice = price;
+
+	}
+
+	// getter for userPrice
+	public double getPrice() {
+		return userPrice;
+	}
+
+}
